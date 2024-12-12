ca1b439fc176755ebee4211a29e2902f76157eef
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-12-12 07:30:23 +0400

    Initial commit changes after creating vite application

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+const theme = createTheme({});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
