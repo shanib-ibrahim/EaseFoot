4a348ab0172cbb1ca3ce728444d5eb2364cd342e
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-12-19 06:51:42 +0400

    change in signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,75 +1,34 @@
-import { Box, FormControl, Stack, TextField, Typography } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
 import styles from "./styles.module.css";
 import { siginLayout } from "../../images";
 
+// Create a styled component for the container box
+const ContainerBoxStyle = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+  border: "1px solid red",
+}));
+
+const InnerBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  margin: "auto",
+  border: "1px solid blue",
+}));
+
 const SignIn = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-        <Box
-          className={styles.leftContainer}
-          sx={{ width: { xs: "100%", sm: "50%" } }}
-        >
-          <Typography
-            className={styles.mainHead}
-            variant="h1"
-            sx={{ fontSize: "34px" }}
-          >
-            Welcome back
-          </Typography>
-          <Typography className={styles.subHead} variant="body1" component="p">
-            Welcome back! Please enter your details.
-          </Typography>
-          <Box>
-            <FormControl fullWidth variant="outlined">
-              <Typography variant="body1" component="span">
-                Email
-              </Typography>
-              <TextField
-                id="email-input"
-                value=""
-                placeholder="Enter Your Email"
-                variant="outlined"
-                fullWidth
-                type="email"
-                sx={{
-                  marginBottom: 2,
-                  paddingTop: "18px",
-                  "& .MuiOutlinedInput-root": {
-                    paddingTop: 0,
-                  },
-                }}
-              />
-              <Typography variant="body1" component="span">
-                Password
-              </Typography>
-              <TextField
-                id="password"
-                value=""
-                placeholder="Enter Your Password"
-                variant="outlined"
-                fullWidth
-                type="password"
-                sx={{
-                  marginBottom: 2,
-                  paddingTop: "18px",
-                  "& .MuiOutlinedInput-root": {
-                    paddingTop: 0,
-                  },
-                }}
-              />
-            </FormControl>
-          </Box>
+    <ContainerBoxStyle>
+      <InnerBox>
+        <Box sx={{ flex: 1, border: "1px solid red", padding: 2 }}>
+          <Typography>Left Information</Typography>
         </Box>
-        <Box sx={{ width: { xs: "100%", sm: "50%" } }}>
-          <img
-            src={siginLayout}
-            alt="A gym builder lifting weights"
-            style={{ width: "100%" }}
-          />
+        <Box sx={{ flex: 1, border: "1px solid red" }}>
+          <img src={siginLayout} alt="A gym builder lifting weights" />
         </Box>
-      </Stack>
-    </Box>
+      </InnerBox>
+    </ContainerBoxStyle>
   );
 };
 
