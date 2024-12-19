ef434e1e41d2b9c3d51dcb3d202fe879a6c1c3f7
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-12-19 07:15:59 +0400

    changes in signin page with layout to understand

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,18 @@
-import { Box, Typography, styled } from "@mui/material";
+import { Box, TextField, Typography, styled } from "@mui/material";
 import styles from "./styles.module.css";
 import { siginLayout } from "../../images";
 
 // Create a styled component for the container box
-const ContainerBoxStyle = styled(Box)(({ theme }) => ({
+const ContainerBoxStyle = styled(Box)(() => ({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   height: "100%",
   border: "1px solid red",
+  fontFamily: "Bree Serif, serif",
 }));
 
-const InnerBox = styled(Box)(({ theme }) => ({
+const InnerBox = styled(Box)(() => ({
   display: "flex",
   margin: "auto",
   border: "1px solid blue",
@@ -22,7 +23,37 @@ const SignIn = () => {
     <ContainerBoxStyle>
       <InnerBox>
         <Box sx={{ flex: 1, border: "1px solid red", padding: 2 }}>
-          <Typography>Left Information</Typography>
+          <Box
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            <Typography className={styles.mainHead}>Welcome Back</Typography>
+            <Typography className={styles.subHead}>
+              Welcome back! Please enter your details.
+            </Typography>
+          </Box>
+          <Box sx={{ border: "1px solid red" }}>
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{
+                fontSize: "14px",
+                fontWeight: "bold",
+                border: "1px solid red",
+              }}
+            >
+              Email
+            </Typography>
+            <TextField
+              type="email"
+              variant="outlined"
+              fullWidth
+              required
+              margin="normal"
+              sx={{ borderRadius: "12px", border: "1px solid blue" }}
+            />
+          </Box>
         </Box>
         <Box sx={{ flex: 1, border: "1px solid red" }}>
           <img src={siginLayout} alt="A gym builder lifting weights" />
